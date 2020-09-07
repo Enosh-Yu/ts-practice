15ee2cdf09a560b96243bbb2d08a766ffa71e92c
유종훈 <yjh@cloubot.com>
2020-09-07 13:50:11 +0900

    test

--- src/Greetings.tsx
+++ src/Greetings.tsx
@@ -15,7 +15,7 @@ function Greetings({ name, mark, optional, onClick }: GreetingsProps) {
   const handleClick = () => onClick(name);
   return (
     <div>
-      Hello, {name} {mark}
+      Hello ..., {name} {mark}
       {optional && <p>{optional}</p>}
       <div>
         <button onClick={handleClick}>Click Me</button>
